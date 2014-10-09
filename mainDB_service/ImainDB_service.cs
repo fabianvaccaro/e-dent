9bbfe8bb55d917885bfd9274366bd540252aca47
fabianvaccaro <fabianvaccaro@hotmail.com>
2014-10-09 17:36:36 +0200

    DB

---
+++ mainDB_service/ImainDB_service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace mainDB_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ImainDB_service" in both code and config file together.
+    [ServiceContract]
+    public interface ImainDB_service
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        String getUsername(Int32 ID);
+
+    }
+}
---
+++ mainDB_service/mainDB_service.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace mainDB_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "mainDB_service" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select mainDB_service.svc or mainDB_service.svc.cs at the Solution Explorer and start debugging.
+    public class mainDB_service : ImainDB_service
+    {
+        public void DoWork()
+        {
+        }
+
+        public String getUsername(Int32 ID)
+        {
+            using (cdentalEntities entidad = new cdentalEntities())
+            {
+                var nombrequery = from user in entidad.users
+                                 where user.usr_id == ID
+                                 select new
+                                 {
+                                     user.username,
+                                     user.role
+                                 };
+
+                //nombrequery.Take(10).Skip(20);,
+                return nombrequery.FirstOrDefault().username;
+            }
+        }
+    }
+}
